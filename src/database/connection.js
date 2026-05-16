7fbbe8622e87cb02329c019d43fc9d000bfc272a
zeyad <aziad711@gmail.com>
2026-05-16 20:08:12 +0300

    ✅ ADD DATABSE CONNECTION

---
+++ src/database/connection.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+ async function connectionOfDatabase() {
+  try {
+    const databaseUrl = "mongodb://localhost:27017/route_task_"
+    await mongoose.connect(databaseUrl,{
+        maxPoolSize:10,
+        serverSelectionTimeoutMS:4000
+    })
+    console.log(`✅ Database is Connected Scuuessfully `);
+  } catch (error) {
+    console.error(`❌ Database connection failed: ${error}`);
+  }
+};
+
+export default connectionOfDatabase;
